7068847c463ff47f4aa245c93e203e0f5b92ebd8
Knowblesse <knowblesse@gmail.com>
2022-03-21 16:48:58 +0900

    install vim plug

--- setVim_linux.sh
+++ setVim_linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Welcome Knowblesse!"
 echo "automatically Setting Vim file for you"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ~/VCF/vimrc/_vimrc ~/.vimrc
 cp ~/VCF/vimrc/monokai.vim ~/.vim/colors/monokai.vim
