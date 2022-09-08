abfb0a5fef36fb1f543b485cac95c4cbf1eea610
Knowblesse <knowblesse@gmail.com>
2022-09-08 15:34:56 +0900

    window vim plug setup

--- setVim_win.bat
+++ setVim_win.bat
@@ -14,6 +14,9 @@ mkdir %userprofile%\vimfiles\colors
 ECHO vimfiles\colors created
 )
 
+::VimPlug Setup
+curl -fLo %userprofile%/vimfiles/autoload/plug.vim --create-dirs --ssl-no-revoke "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+
 ::Copy colors
 copy C:\VCF\vimrc\monokai.vim %userprofile%\vimfiles\colors\monokai.vim
 ECHO monokai color added
