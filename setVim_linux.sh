eb4beb73adc48f425cf1fe02f0034f0a9ff9ade1
Knowblesse <knowblesse@gmail.com>
2023-05-16 11:06:22 +0900

    mkdir error fixed

--- setVim_linux.sh
+++ setVim_linux.sh
@@ -4,4 +4,6 @@ echo "automatically Setting Vim file for you"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ~/VCF/vimrc/_vimrc ~/.vimrc
+mkdir -p ~/.vim
+mkdir -p ~/.vim/colors
 cp ~/VCF/vimrc/monokai.vim ~/.vim/colors/monokai.vim
